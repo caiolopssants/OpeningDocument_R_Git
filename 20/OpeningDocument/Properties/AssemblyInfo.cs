c33a2f7cb233375f04112ddd425da3d1216a71a2
caio deodato santos <caiodeodato@hotmail.com>
2021-01-14 03:22:00 -0300

    init

---
+++ 20/OpeningDocument/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("OpeningDocument")]
+[assembly: AssemblyDescription("Open any Revit document")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("caiolopssants")]
+[assembly: AssemblyProduct("OpeningDocument")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("caiolopssants")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("f1fbc46e-84d9-4d33-a7c7-0818edd7bce1")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o '*' como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
---
+++ 21/OpeningDocument/Command.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Windows.Forms;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+namespace OpeningDocument
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            using (OpenFileDialog oFD = new OpenFileDialog {Multiselect = true, Filter = "Revit Project/ Family|*.rvt;*.rte;*.rfa;*.rft"})
+            {   
+                if (oFD.ShowDialog() == DialogResult.OK && oFD.FileNames.Length > 0)
+                {
+                    DialogResult dialogResult = oFD.FileNames.Any(path => Path.GetExtension(path) == ".rvt") ? MessageBox.Show("Do you wanna open like a detached document?", "Opening document mode", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) : DialogResult.None;
+                    if (dialogResult == DialogResult.Cancel) { return Result.Cancelled; }
+                    DialogResult discartWorksetDialogResult = dialogResult == DialogResult.Yes ? MessageBox.Show("Do you wanna preserve the document worksets", "Workset preservation", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) : DialogResult.None;
+                    if (discartWorksetDialogResult == DialogResult.Cancel) { return Result.Cancelled; }
+
+                    EventHandler<DialogBoxShowingEventArgs>  eventHandler = SetPopUpEvent(commandData);
+                    foreach (string path in oFD.FileNames)
+                    {
+                        try
+                        {
+                            DetachFromCentralOption detachFromCentral = (dialogResult == DialogResult.Yes && Path.GetExtension(path) == ".rvt") ?
+                                                                            (discartWorksetDialogResult == DialogResult.Yes) ?
+                                                                                DetachFromCentralOption.DetachAndPreserveWorksets :
+                                                                                    DetachFromCentralOption.DetachAndDiscardWorksets :
+                                                                                        DetachFromCentralOption.DoNotDetach;
+                            commandData.Application.OpenAndActivateDocument(ModelPathUtils.ConvertUserVisiblePathToModelPath(path),
+                                new OpenOptions
+                                {
+                                    AllowOpeningLocalByWrongUser = true,
+                                    DetachFromCentralOption = detachFromCentral
+                                },
+                                true);
+                        }
+                        catch(Exception error)
+                        {
+                            MessageBox.Show($"Error: {error.Message}\nTime: {DateTime.Now.ToString()}");
+                        }
+                    }
+                    RemovePopUpEvent(commandData, eventHandler);
+                }
+            }
+                return Result.Succeeded;
+        }
+        private void RemovePopUpEvent(ExternalCommandData commandData, EventHandler<DialogBoxShowingEventArgs> eventHandler) => commandData.Application.DialogBoxShowing -= eventHandler;
+        private EventHandler<DialogBoxShowingEventArgs> SetPopUpEvent(ExternalCommandData commandData)
+        {
+            EventHandler < DialogBoxShowingEventArgs > eventHandler = new EventHandler<DialogBoxShowingEventArgs>(PopUpControlEvent);
+            commandData.Application.DialogBoxShowing += eventHandler;
+            return eventHandler;
+            void PopUpControlEvent(object sender, DialogBoxShowingEventArgs e)
+            {
+                if (!e.OverrideResult((int)TaskDialogResult.Ok))
+                {
+                    e.OverrideResult((int)TaskDialogResult.Cancel);
+                    MessageBox.Show($"Canceled process because of this message: {((TaskDialogShowingEventArgs)e).Message}", "Process canceled");
+                }
+                #region Code disabled
+                //Autodesk.Revit.UI.Events.TaskDialogShowingEventArgs e1 = e as Autodesk.Revit.UI.Events.TaskDialogShowingEventArgs;
+                //File.AppendAllText(@"G:\messages.txt",
+                //    $"Message: \n" +
+                //    $"Cancellable: {e.Cancellable}\n" +
+                //    $"DialogId: {e.DialogId}\n" +
+                //    $"IsValidObject: {e.IsValidObject}\n" +                
+                //    $"-------------------------------------------\n" +
+                //    $"Message: {e1.Message}\n" +
+                //    $"Cancellable: {e.Cancellable}\n" +
+                //    $"DialogId: {e.DialogId}\n" +
+                //    $"IsValidObject: {e.IsValidObject}\n" +
+                //    $"\n\n\n\n");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.Cancel)} 1");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.Close)} 2");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.CommandLink1)} 3");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.CommandLink2)} 4");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.CommandLink3)} 5");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.CommandLink4)} 6");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.No)} 7");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.None)} 8");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.Ok)} 9");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.Retry)} 10");
+                //MessageBox.Show($"{e.OverrideResult((int)TaskDialogResult.Yes)} 11");
+                #endregion
+            }
+        }
+       
+    }
+}
